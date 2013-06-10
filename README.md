c8f99b7f7525d1f428902cea3f8e3910c6234abf
Max Schaefer <schaefer@ntu.edu.sg>
2013-06-10 09:38:36 +0800

    Copying over proxy.js from Eavesdropper project.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 rewriting-proxy
 ===============
 
-Simple proxy for rewriting JavaScript on-the-fly
+Simple proxy for rewriting JavaScript on-the-fly.
+
+
+License
+-------
+
+This software is distributed under the [Eclipse Public License](http://www.eclipse.org/legal/epl-v10.html).
---
+++ proxy.js
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Max Schaefer.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Max Schaefer - initial API and implementation
+ *******************************************************************************/
+
+/*global require console Buffer __dirname process*/
+
+var http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    url = require('url'),
+    util = require('util');
+    
+if(process.argv.length <= 2) {
+	console.error("Usage: node proxy.js REWRITER");
+	process.exit(-1);
+}
+
+var rewriter = require(process.argv[2]);
+
+http.createServer(function(request, response) {
+    var proxy = http.createClient(80, request.headers.host);
+    
+    delete request.headers['accept-encoding'];
+    console.log("requesting " + request.url);
+    var proxy_request = proxy.request(request.method, request.url, request.headers);
+    
+    var url_path = url.parse(request.url).pathname;
+    
+    proxy_request.addListener('response', function (proxy_response) {
+		var tp = proxy_response.headers['content-type'] || "", buf = "";
+		if(tp.match(/JavaScript/i) || tp.match(/text/i) && url_path.match(/\.js$/i))
+			tp = "JavaScript";
+		else if(tp.match(/HTML/i))
+			tp = "HTML";
+		else
+			tp = "other";
+      
+		proxy_response.addListener('data', function(chunk) {
+			if(tp !== "JavaScript")
+				response.write(chunk, 'binary');
+			else
+				buf += chunk.toString();
+		});
+		
+		proxy_response.addListener('end', function() {
+			if(tp === "JavaScript") {
+				var output;
+				try {
+					var file = path.basename(url_path);
+					output = rewriter.rewrite(buf, { type: 'script', url: request.url });
+					console.log("Successfully instrumented " + request.url);
+				} catch(e) {
+					console.warn("Couldn't parse " + request.url + " as JavaScript; passing on un-instrumented");
+					output = buf;
+				}
+			    proxy_response.headers['content-length'] = Buffer.byteLength(output, 'utf-8');
+			    response.writeHead(proxy_response.statusCode, proxy_response.headers);
+			    response.write(output);
+		    }
+		    response.end();
+		});
+		
+		if(tp !== 'JavaScript')
+			response.writeHead(proxy_response.statusCode, proxy_response.headers);
+    });
+
+    request.addListener('data', function(chunk) {
+		proxy_request.write(chunk, 'binary');
+    });
+
+    request.addListener('end', function() {
+		proxy_request.end();
+    });
+}).listen(8080);
+
+console.log("Listening on port 8080");
