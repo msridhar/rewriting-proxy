a298c7e2de86b9ee44400133882e24f09107c676
Manu Sridharan <m.sridharan@samsung.com>
2013-11-19 15:03:18 -0800

    just change indentation

--- proxy.js
+++ proxy.js
@@ -8,56 +8,50 @@
  * Contributors:
  *     Max Schaefer - initial API and implementation
  *******************************************************************************/
-
 /*jslint node: true */
 /*global require console Buffer __dirname process*/
-
 var http = require('http'),
-    path = require('path'),
-    urlparser = require('url'),
-    htmlparser = require('htmlparser'),
-    htmlparser2html = require('htmlparser-to-html'),
-    Entities = require('html-entities').AllHtmlEntities,
-    assert = require("assert");
-    
-
+	path = require('path'),
+	urlparser = require('url'),
+	htmlparser = require('htmlparser'),
+	htmlparser2html = require('htmlparser-to-html'),
+	Entities = require('html-entities').AllHtmlEntities,
+	assert = require("assert");
 var entities = new Entities();
-    
 var unparseable_count = 0;
 
 function rewriteScript(src, metadata, rewriteFunc) {
 	var result;
-		
 	var prefix = "";
-	if(src.match(/^javascript:/i)) {
-		prefix = src.substring(0, "javascript".length+1);
+	if (src.match(/^javascript:/i)) {
+		prefix = src.substring(0, "javascript".length + 1);
 		src = src.substring(prefix.length);
 	}
 	try {
-		result = rewriteFunc(src, metadata);			
+		result = rewriteFunc(src, metadata);
 	} catch (e) {
 		console.log("exception while rewriting script " + metadata.url);
 		console.log(e);
 		return src;
 	}
-	result = prefix+result;
+	result = prefix + result;
 	return result;
 }
 
-var script_counter = 0, event_handler_counter = 0, js_url_counter = 0;
-
+var script_counter = 0,
+	event_handler_counter = 0,
+	js_url_counter = 0;
 // event handler attributes
-var event_handler_attribute_names = [ "onabort", "onblur", "onchange", "onclick", "ondblclick",
-                                      "onerror", "onfocus", "onkeydown", "onkeypress", "onkeyup",
-                                      "onload", "onmousedown", "onmousemove", "onmouseout", "onmouseover",
-                                      "onmouseup", "onreset", "onresize", "onselect", "onsubmit", "onunload" ];
-                                      
+var event_handler_attribute_names = ["onabort", "onblur", "onchange", "onclick", "ondblclick",
+	"onerror", "onfocus", "onkeydown", "onkeypress", "onkeyup",
+	"onload", "onmousedown", "onmousemove", "onmouseout", "onmouseover",
+	"onmouseup", "onreset", "onresize", "onselect", "onsubmit", "onunload"
+];
 // attributes that may contain URLs (unsure whether all of these can actually contain 'javascript:' URLs)
-var url_attribute_names = [ "action", "cite", "code", "codebase", "data", "href", "manifest", "poster", "src" ];
+var url_attribute_names = ["action", "cite", "code", "codebase", "data", "href", "manifest", "poster", "src"];
 
 function walkDOM(node, url, rewriteFunc, headerCode) {
 	var src, metadata;
-
 	if (node.name === 'head' && headerCode) {
 		// try sticking a script at the beginning
 		var newScript = {
@@ -65,11 +59,11 @@ function walkDOM(node, url, rewriteFunc, headerCode) {
 			data: 'script',
 			type: 'script',
 			name: 'script',
-			children: [ {
-				type: 'text', 
+			children: [{
+				type: 'text',
 				raw: headerCode,
 				data: headerCode
-			} ]
+			}]
 		};
 		if (node.children) {
 			node.children.unshift(newScript);
@@ -85,53 +79,63 @@ function walkDOM(node, url, rewriteFunc, headerCode) {
 		return;
 	}
 	// handle scripts (but skip empty ones)
-	if(node.children && node.type === 'script') {
+	if (node.children && node.type === 'script') {
 		// scripts without type are assumed to contain JavaScript
-		if(!node.attribs || !node.attribs.type || node.attribs.type.match(/JavaScript/i)) {
+		if (!node.attribs || !node.attribs.type || node.attribs.type.match(/JavaScript/i)) {
 			// only rewrite inline scripts; external scripts are handled by request rewriting
-			if(!node.attribs || !node.attribs.src) {
+			if (!node.attribs || !node.attribs.src) {
 				src = "";
-				for(var i=0,n=node.children.length;i<n;++i) {
-					if(node.children[i].type === 'text' || node.children[i].type === 'comment') {						
+				for (var i = 0, n = node.children.length; i < n; ++i) {
+					if (node.children[i].type === 'text' || node.children[i].type === 'comment') {
 						src += node.children[i].raw;
 					} else {
 						throw new Error("script has child of type " + node.children[i].type + "; that's not supposed to happen");
 					}
 				}
-				metadata = { type: 'script', inline: true, url: url + "#inline-" + (script_counter++) };
-				
+				metadata = {
+					type: 'script',
+					inline: true,
+					url: url + "#inline-" + (script_counter++)
+				};
 				node.children.length = 1;
 				node.children[0].type = 'text';
 				node.children[0].raw = node.children[0].data = rewriteScript(src, metadata, rewriteFunc);
 			}
 		}
 	}
-	
 	// handle event handlers and 'javascript:' URLs
-	if(node.attribs) {
-		for(var attrib in node.attribs) {
-			if(node.attribs.hasOwnProperty(attrib)) {
-				if(event_handler_attribute_names.indexOf(attrib.toLowerCase()) !== -1) {
+	if (node.attribs) {
+		for (var attrib in node.attribs) {
+			if (node.attribs.hasOwnProperty(attrib)) {
+				if (event_handler_attribute_names.indexOf(attrib.toLowerCase()) !== -1) {
 					src = entities.decode(String(node.attribs[attrib]));
-					metadata = { type: 'event-handler', url: url + "#event-handler-" + (event_handler_counter++) };
+					metadata = {
+						type: 'event-handler',
+						url: url + "#event-handler-" + (event_handler_counter++)
+					};
 					// since htmlparser2html does its own encoding of attribute values, here we 
 					// just remove newlines and leave the decoded version in the tree
 					var rewritten = rewriteScript(src, metadata, rewriteFunc).replace("\n", "");
 					node.attribs[attrib] = rewritten;
-				} else if(url_attribute_names.indexOf(attrib) !== -1 && String(node.attribs[attrib]).match(/^javascript:/i)) {
+				} else if (url_attribute_names.indexOf(attrib) !== -1 && String(node.attribs[attrib]).match(/^javascript:/i)) {
 					src = entities.decode(String(node.attribs[attrib]));
-					metadata = { type: 'javascript-url', url: url + "#js-url-" + (js_url_counter++) };
+					metadata = {
+						type: 'javascript-url',
+						url: url + "#js-url-" + (js_url_counter++)
+					};
 					node.attribs[attrib] = entities.encode(rewriteScript(src, metadata, rewriteFunc));
 				}
 			}
 		}
 	}
-	
-	if(node.type === 'tag' && node.children) {
-		node.children.forEach(function(ch) { walkDOM(ch, url, rewriteFunc, headerCode); });
-	}
-	else if(Array.isArray(node)) {
-		node.forEach(function(ch) { walkDOM(ch, url, rewriteFunc, headerCode); });
+	if (node.type === 'tag' && node.children) {
+		node.children.forEach(function (ch) {
+			walkDOM(ch, url, rewriteFunc, headerCode);
+		});
+	} else if (Array.isArray(node)) {
+		node.forEach(function (ch) {
+			walkDOM(ch, url, rewriteFunc, headerCode);
+		});
 	}
 }
 
@@ -163,10 +167,9 @@ function start(options) {
 	assert(options.rewriter, "must provide rewriter function in options.rewriter");
 	var headerCode = options.headerCode;
 	var rewriteFunc = options.rewriter;
-	server = http.createServer(function(request, response) {
-	    // make sure we won't get back gzipped stuff
-	    delete request.headers['accept-encoding'];
-		
+	server = http.createServer(function (request, response) {
+		// make sure we won't get back gzipped stuff
+		delete request.headers['accept-encoding'];
 		console.log("request: " + request.url);
 		var parsed = urlparser.parse(request.url);
 		var http_request_options = {
@@ -176,63 +179,60 @@ function start(options) {
 			method: request.method,
 			headers: request.headers
 		};
-		var proxyRequest = http.request(http_request_options, function(proxy_response) {
-			var tp = proxy_response.headers['content-type'] || "", buf = "";
+		var proxyRequest = http.request(http_request_options, function (proxy_response) {
+			var tp = proxy_response.headers['content-type'] || "",
+				buf = "";
 			var url_path = parsed.pathname;
-			
-			if(tp.match(/JavaScript/i) || tp.match(/text/i) && url_path.match(/\.js$/i)) {
+			if (tp.match(/JavaScript/i) || tp.match(/text/i) && url_path.match(/\.js$/i)) {
 				tp = "JavaScript";
-			} else if(tp.match(/HTML/i)) {
+			} else if (tp.match(/HTML/i)) {
 				tp = "HTML";
 			} else {
 				tp = "other";
 			}
-	      
-			proxy_response.on('data', function(chunk) {
-				if(tp === "other") {
+			proxy_response.on('data', function (chunk) {
+				if (tp === "other") {
 					response.write(chunk, 'binary');
 				} else {
 					buf += chunk.toString();
 				}
 			});
-			
-			proxy_response.on('end', function() {
+			proxy_response.on('end', function () {
 				var output;
-				if(tp === "JavaScript") {
-					output = rewriteScript(buf, { type: 'script', inline: false, url: request.url, source: buf }, rewriteFunc);
-			    } else if(tp === "HTML") {
-			        output = rewriteHTML(buf, request.url, rewriteFunc, headerCode);
-			    }
-			    
-			    if(output) {
-				    proxy_response.headers['content-length'] = Buffer.byteLength(output, 'utf-8');
-				    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-				    response.write(output);
+				if (tp === "JavaScript") {
+					output = rewriteScript(buf, {
+						type: 'script',
+						inline: false,
+						url: request.url,
+						source: buf
+					}, rewriteFunc);
+				} else if (tp === "HTML") {
+					output = rewriteHTML(buf, request.url, rewriteFunc, headerCode);
 				}
-				
-			    response.end();
+				if (output) {
+					proxy_response.headers['content-length'] = Buffer.byteLength(output, 'utf-8');
+					response.writeHead(proxy_response.statusCode, proxy_response.headers);
+					response.write(output);
+				}
+				response.end();
 			});
-			
-			if(tp === "other") {
+			if (tp === "other") {
 				response.writeHead(proxy_response.statusCode, proxy_response.headers);
 			}
 		});
 		proxyRequest.on('error', function (e) {
 			console.log("request error " + e.message);
 		});
-	    request.on('data', function(chunk) {
+		request.on('data', function (chunk) {
 			proxyRequest.write(chunk, 'binary');
-	    });
-	
-	    request.on('end', function() {
+		});
+		request.on('end', function () {
 			proxyRequest.end();
-	    });
+		});
 	});
 	var port = options.port ? options.port : 8080;
 	server.listen(port);
 	console.log("listening on port " + port);
 }
-
 exports.start = start;
 exports.rewriteHTML = rewriteHTML;
-// TODO tabs to spaces
